3bd3679771634d8760760e6b7149b152b41a958b
Ahmad Kayyali <kayyali18@gmail.com>
2017-11-01 22:06:58 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require ('mongodb');
+
 var express = require ('express');
 var bodyParser = require ('body-parser');
 
@@ -29,6 +31,55 @@ app.get ('/todos', (req, res) => {
     res.send ({todos});
   }, (e) => {
     res.status (400).send (e);
+  });
+});
+
+//GET request /todos/123(id) --make it dynamic
+//params is an object with key value and key is param in url
+// app.get ('/todos/:id', (req, res) => {
+//   let id = req.params.id;
+//
+//   //validate id using isValid
+//     //not valid give 404 response
+//   if (!ObjectID.isValid (id)) {
+//     res.status (404).send ()
+//   }
+//   // findById
+//   Todo.findById (id).then ((todo) => {
+//     //success
+//       //if todo - send it back
+//     if (todo) {
+//       res.send (todo);
+//     }
+//     // if no todo - send back 404 with empty body
+//     if (!todo) {
+//       res.status (404);
+//       res.send (todo);
+//     }
+//   }).catch ((e) => {
+//     //error
+//       // send back 400 req was not valid send back nothing
+//       res.status (400);
+//   });
+// });
+
+//Andrew's Work
+
+app.get ('/todos/:id', (req, res) => {
+  let id = req.params.id;
+
+  if (!ObjectID.isValid (id)) {
+    return res.status (404).send ();
+  }
+
+  Todo.findById (id).then ((todo) => {
+    if (!todo) {
+      return res.status (404).send ();
+    }
+
+    res.send ({todo});
+  }).catch ((e) => {
+    res.status (400).send ();
   });
 });
 
